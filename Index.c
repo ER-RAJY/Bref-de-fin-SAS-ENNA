9b8d67c97a0fb8dd26b2525f730a85cc59ba7e13
ER-RAJY <102237902+ER-RAJY@users.noreply.github.com>
2024-01-31 15:09:37 +0100

    Update Index.c

--- Index.c
+++ Index.c
@@ -3,8 +3,7 @@
 
 //Global Variables
 int NMBR_T = 0;
-
-enum priority{
+enum Priorite{
 	 HAUTE ,
 	 MOYENNE ,
 	 BASSE ,
@@ -20,12 +19,12 @@ struct date {
 typedef struct {
 	char tache_name[50];
 	char description[100];
-	int  priority;
+	enum Priorite  priority;
 	struct date tacheDate;
 }Taches;
 
-//founction de ajouter des taches 
-void Ajouter( Taches tache[]) {
+//founction de ajouter Une taches 
+void AjouterUnTache( Taches tache[]) {
     printf("Le nom de tache:\n ");
     scanf(" %[^\n]s", tache[NMBR_T].tache_name);
 
@@ -38,17 +37,44 @@ void Ajouter( Taches tache[]) {
     printf("+ La Date (format: day/month/year) :\n");
     scanf("%d/%d/%d", &tache[NMBR_T].tacheDate.day, &tache[NMBR_T].tacheDate.month, &tache[NMBR_T].tacheDate.year);
 
-    // Clear the input buffer
+
     while (getchar() != '\n');
 
 	// pour incrementer la valeur de Nombre des taches
     NMBR_T++;
 }
-	
+//founction de ajouter des taches 
+void AjouterPlusieurs(Taches tache[], int nombreTaches) {
+    if (NMBR_T + nombreTaches > 50) {
+        printf("Il n'y a pas assez d'espace pour ajouter autant de taches.\n");
+    } else {
+        for (int i = 0; i < nombreTaches; i++) {
+            printf("Tache %d\n",i+1);
+            printf("Le nom de la tache :\n ");
+            scanf(" %[^\n]s", tache[NMBR_T + i].tache_name);
+
+            printf("+ La Description :\n");
+            scanf(" %[^\n]s", tache[NMBR_T + i].description);
+
+            printf("La Priorite (0: HAUTE, 1: MOYENNE, 2: BASSE) : ");
+            scanf("%d", &tache[NMBR_T + i].priority);
+            
+            printf("+ La Date (format: day/month/year) :\n");
+            scanf("%d/%d/%d", &tache[NMBR_T + i].tacheDate.day, &tache[NMBR_T + i].tacheDate.month, &tache[NMBR_T + i].tacheDate.year);
+
+
+            while (getchar() != '\n');
+        }
+
+        NMBR_T += nombreTaches;
+    }
+}
+
+
 //La fonction de Affichage 
 void Affichage( Taches tache[]) {
     if (NMBR_T < 1) {
-        printf("There are no tasks to display.\n");
+        printf("Pas des taches a afficher\n");
     } else {
         printf("\n\t\t\t\t\t\t:::::::::::::::::::::::::");
         printf("\n\t\t\t\t\t\t\tTO DO LIST");
@@ -72,13 +98,13 @@ void Modifier( Taches tache[], int N) {
     char NewDescription[100];
     int priorite;
     struct date newDate;
-
-    if (N >= 1 && N <= NMBR_T) {
+  if (N >= 1 && N <= NMBR_T) {
         printf("::::::::: Que voulez vous modifier :::::::\t\n");
         printf(":: < 1 >  Le nom de tache               ::\n");
         printf(":: < 2 >  La description                ::\n");
         printf(":: < 3 >  La priorite.                  ::\n");
         printf(":: < 4 >  La date                       ::\n");
+        printf(":: < 0 >  Retour/Menu precedent         ::\n");
         printf("::::::::::::::::::::::::::::::::::::::::::\t\n");
         printf("\n");
 
@@ -86,6 +112,10 @@ void Modifier( Taches tache[], int N) {
         scanf("%d", &choix);
 
         switch (choix) {
+                case 0:
+                printf("Vous avez quitte !\n");
+                break;
+
             case 1:
                 printf("Entrer le nouveau nom de la tache : ");
                 scanf(" %[^\n]s", Newname);
@@ -117,21 +147,20 @@ void Modifier( Taches tache[], int N) {
             default:
                 printf("Choix invalide.\n");
                 break;
-        }
-    } else {
+        }while (choix!=0);
+        
+    } 
+    else {
         printf("Numero de tache invalide.\n");
     }
 }
 
 //La function supprimer
-void Supprimer( Taches tache[]) {
+void Supprimer( Taches tache[]) {  
     int pos;
-
     printf("Entrez le ID de la tache a supprimer : ");
     scanf("%d", &pos);
-
     // Validation 
-
     if (pos >= 1 && pos <= NMBR_T) {
         for (int i = pos - 1; i < NMBR_T - 1; i++) {
             tache[i] = tache[i + 1];
@@ -166,15 +195,14 @@ void trieeCroissant( Taches tache[]) {
 }
 
 //La function de triee decroissant
-void trieeDecroissant( Taches tache[]) {
+void trieeDecroissant(Taches tache[]) {
     if (NMBR_T > 0) {
         for (int i = 0; i < NMBR_T; i++) {
-            for (int j = i - 1; j < NMBR_T; j++) {
-                // Comparer les annee et le moins et le jours
-
-                if (tache[i].tacheDate.year > tache[j].tacheDate.year || (tache[i].tacheDate.year == tache[j].tacheDate.year && tache[i].tacheDate.month > tache[j].tacheDate.month) 
-                || (tache[i].tacheDate.year == tache[j].tacheDate.year && tache[i].tacheDate.month == tache[j].tacheDate.month && tache[i].tacheDate.day > tache[j].tacheDate.day))
-                 {
+            for (int j = i + 1; j < NMBR_T; j++) {
+                // Comparer les annee et le mois et le jour
+                if (tache[i].tacheDate.year < tache[j].tacheDate.year || 
+                    (tache[i].tacheDate.year == tache[j].tacheDate.year && tache[i].tacheDate.month < tache[j].tacheDate.month) ||
+                    (tache[i].tacheDate.year == tache[j].tacheDate.year && tache[i].tacheDate.month == tache[j].tacheDate.month && tache[i].tacheDate.day < tache[j].tacheDate.day)) {
                     // changer les elements
                     Taches temp = tache[i];
                     tache[i] = tache[j];
@@ -182,7 +210,8 @@ void trieeDecroissant( Taches tache[]) {
                 }
             }
         }
-    }Affichage(tache);
+    }
+    Affichage(tache);
 }
 
 void FiltrerParPriorite( Taches tache[]) {
@@ -212,7 +241,7 @@ void FiltrerParPriorite( Taches tache[]) {
                 case MOYENNE:
                     printf(" MOYENNE          |\n");
                     break;
-                case BASSE:
+                case BASSE:!
                     printf(" BASSE            |\n");
                     break;
                 default:
@@ -232,11 +261,9 @@ void FiltrerParPriorite( Taches tache[]) {
 
 //La function de Menu
 void Menu( Taches tache[]) {
-    int  choix;
-	// printf("Entrer le nombre de tache :\n");
-    // scanf("")
-	printf("\n");
 
+    int  choix;
+	printf("\n");
 	do{
 		printf(":::::::::::::::::< Menu >:::::::::::::::::\t\n");
 		printf(":: < 1 >  Ajouter une Tache.            ::\n");
@@ -252,27 +279,58 @@ void Menu( Taches tache[]) {
 		scanf("%d",&choix);
 		
 		switch(choix){
-			case 1:
-            //Ajouter 
-				Ajouter(tache);
-				break;
-				
+         int nombreTaches;
+int nbr;
+
+case 1:
+    // Ajouter
+    printf("(1) Ajouter une tache.\n");
+    printf("(2) Ajouter plusieurs taches.\n");
+    printf("==> Saisir Votre Choix : ");
+    scanf("%d", &nbr);
+
+    switch (nbr) {
+        case 1:
+            AjouterUnTache(tache);
+            break;
+        case 2:
+            printf("Entrez le nombre de taches a ajouter : ");
+            scanf("%d", &nombreTaches);
+
+            // Call AjouterPlusieurs
+            AjouterPlusieurs(tache, nombreTaches);
+            break;
+
+        default:
+            printf("Choix invalide. Entrer un choix entre 1 et 2!\n");
+            break;
+    }
+    break;
+
 			case 2:
             //Affichage 
 				Affichage(tache);
 				break;
 		    case 3: {
                 // Modifier
-                int N;
+                if (NMBR_T < 1) {
+                    printf("\t\"Pas des taches pour modifier!!\"\n");
+                    break;
+                }
+                else{int N;
                 Affichage(tache);
                 printf("Entrer le ID de la tache : ");
                 scanf("%d", &N);
                 Modifier(tache, N);
-                break;
+                break;}
             }
 				 break;
 			case 4:{
             //Supprimer
+            if (NMBR_T < 1) {
+                    printf("\t\"Pas des taches pour Supprimer!!\"\n");
+                    break;
+                }else
                 Affichage(tache);
 				Supprimer(tache);
 				 break;
@@ -280,8 +338,12 @@ void Menu( Taches tache[]) {
 
 			case 5:{
             //Triee
-                int Triee ; 
-                printf("croissant (0) \t decroissant (1) \n");
+              if (NMBR_T < 1) {
+                    printf("\t\"Pas des taches a Ordoner!!\"\n");
+                    break;
+                }else{
+                    int Triee ; 
+                printf("croissant (1) \t decroissant (2) \n");
                 printf("=> Saisir Votre Choix : ");
                 scanf("%d",&Triee);
                 switch (Triee)
@@ -299,26 +361,28 @@ void Menu( Taches tache[]) {
                     break;
                 }
 				 break;
+                }  
             } 
             case 6:    
                 //Filtrer Par Priorite
-                
+                if (NMBR_T < 1) {
+                    printf("\t\"Pas des taches a filtrer!!\"\n");
+                    break;
+                }else
                 FiltrerParPriorite(tache);
                 break;	  
-
+            case 7:
+                printf("Programme termine.\n");
+                break;	
 			default:
 				printf("Choix invalide. Entrer un choix entre 1 et 7!\n");
-			case 7:
-                printf("Programme terminé.\n");
-                break;	
+		
 		}
-	} while( choix != '7');			
+	} while( choix != 7);
 }
 
-int main(){
-	
+int main(){	
 	char choix;
 	Taches task[50];
 	Menu(task);	
-		
 }
